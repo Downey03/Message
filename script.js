9a8f31568e5e36f06f065ed961568855173d7cdd
Downey03 <44365074+Downey03@users.noreply.github.com>
2023-12-09 19:36:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yaaaaay! Where? When?";
+  question.innerHTML = "Yaaaaay!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
